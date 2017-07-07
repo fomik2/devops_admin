4d1dd2a0249611e1d851efd5db26c97327b6b14e
root <root@localhost.localdomain>
2017-07-07 23:51:20 +0300

    test

---
+++ test_module/manifests/init.pp
@@ -0,0 +1,14 @@
+
+class test_module {
+
+$hosts = foreman("hostgroup=devops", "state=all")  
+#$hosts = foreman("fact=operatingsystem=Centos", "state=all")
+
+
+  #$hosts = [1,2,3]
+
+  file {'/tmp/test':
+    ensure  => present, 
+    content => template('/etc/puppetlabs/test.erb'),
+  }
+}
