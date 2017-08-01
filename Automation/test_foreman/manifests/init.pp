eccd1425b4e3590c880a6ceb69a6ff3689cc342a
root <root@localhost.localdomain>
2017-08-01 08:28:21 +0300

    after puppet lint

--- Automation/test_foreman/manifests/init.pp
+++ Automation/test_foreman/manifests/init.pp
@@ -1,7 +1,7 @@
 # Test_foreman class make query to Foreman database and fetch any information like ip, hostname and etc.
 
 class test_foreman (
-  
+
   $item = $test_foreman::params::item,
   $search = $test_foreman::params::search,
   $foreman_url = $test_foreman::params::foreman_url,
@@ -9,20 +9,19 @@ class test_foreman (
   $foreman_pass = $test_foreman::params::foreman_pass
 
 ) inherits test_foreman::params {
-  
- 
- 
- $query = { item         => "${item}",
-            search       => "${search}",
-            foreman_url  => "${foreman_url}",
-            foreman_user => "${foreman_user}",
-            foreman_pass => "${foreman_pass}" }
+
+
+  $query = { item  => $item,
+    search         => $search,
+    foreman_url    => $foreman_url,
+    foreman_user   => $foreman_user,
+    foreman_pass   => $foreman_pass
+  }
 
   $result = foreman($query)['results']
 
-  
   file {'/tmp/test.conf':
-    ensure   => present,
+    ensure  => present,
     content => template('test_foreman/test.erb')
   }
 
--- Automation/test_foreman/manifests/params.pp
+++ Automation/test_foreman/manifests/params.pp
@@ -1,9 +1,11 @@
+# Params for test foreman function: you can assign password, login and search string.
+
 class test_foreman::params {
-  
+
   $item         = 'hosts'
   $search       = 'hostgroup = Jenkins'
   $foreman_url  = 'https://foreman.test.com'
-  $foreman_user = 'YOUR_USERNAME_HERE'
-  $foreman_pass = 'YOUR_PASS_HERE'
+  $foreman_user = 'YOUR LOGIN HERE'
+  $foreman_pass = 'YOUR PASS HERE'
 
 }
