a9bffbd2b886cf5fb0439290988d54921b87afaf
root <root@localhost.localdomain>
2017-08-03 15:25:02 +0300

    test_foreman spec

--- Automation/test_foreman/manifests/params.pp
+++ Automation/test_foreman/manifests/params.pp
@@ -5,7 +5,7 @@ class test_foreman::params {
   $item         = 'hosts'
   $search       = 'hostgroup = Jenkins'
   $foreman_url  = 'https://foreman.test.com'
-  $foreman_user = 'YOUR LOGIN HERE'
-  $foreman_pass = 'YOUR PASS HERE'
+  $foreman_user = 'admin'
+  $foreman_pass = '20hRKS97'
 
 }
--- Automation/test_foreman/spec/classes/init_spec.rb
+++ Automation/test_foreman/spec/classes/init_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
+
 describe 'test_foreman' do
-  context 'with default values for all parameters' do
-    it { should contain_class('test_foreman') }
-  end
+  let(:title) { 'test_foreman' }
+
+  let(:params) { {
+    :item         => 'hosts',
+    :search       => 'hostgroup = Jenkins',
+    :foreman_url  => 'https://foreman.test.com',
+    :foreman_user => 'admin',
+    :foreman_pass => '20hRKS97'
+  } }
+
+
+  it { is_expected.to contain_file('/tmp/test.conf') }
+
 end
